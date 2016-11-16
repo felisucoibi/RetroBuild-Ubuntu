854e931d0d96d9ec4c445c97bdb3320ef5de725e
felisucoibi <info@justdust.es>
2016-11-16 12:53:16 +0100

    UPDATED

--- rsbuild.sh
+++ rsbuild.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# JUST A SMALL SCRIPT TO ONE CLICK UBUNTU BUILDING OF RETROSHARE.ORG
+
 if [ ! -d "./retroshare" ]; then
   sudo apt-get install git
   sudo apt-get install ibglib2.0-dev libupnp-dev libssl-dev libxss-dev libgnome-keyring-dev libbz2-dev libspeex-dev libspeexdsp-dev libxslt1-dev cmake libcurl4-openssl-dev libopencv-dev tcl8.6 libsqlcipher-dev libmicrohttpd-dev libavcodec-dev qtmultimedia5-dev qttools5-dev libqt5x11extras5-dev qt5-default
