3ee3878886a83daeb46d8af6dcad1fa9d3a63f85
felisucoibi <info@justdust.es>
2017-06-16 14:18:56 +0200

    changed to main rs build

--- rsbuild.sh
+++ rsbuild.sh
@@ -6,11 +6,11 @@ if [ ! -d "./retroshare" ]; then
   sudo apt-get install ibglib2.0-dev libupnp-dev libssl-dev libxss-dev libgnome-keyring-dev libbz2-dev libspeex-dev libspeexdsp-dev libxslt1-dev cmake libcurl4-openssl-dev libopencv-dev tcl8.6 libsqlcipher-dev libmicrohttpd-dev libavcodec-dev qtmultimedia5-dev qttools5-dev libqt5x11extras5-dev qt5-default
   mkdir ./retroshare
   cd ./retroshare
-  git clone https://github.com/felisucoibi/RetroGhibli.git master
+  git clone https://github.com/RetroShare/RetroShare.git master
   cd ..
 fi
 cd ./retroshare/master
-git pull https://github.com/felisucoibi/RetroGhibli.git master
+git pull https://github.com/RetroShare/RetroShare.git master
 # git push origin master
 qmake CONFIG+=debug
 make
